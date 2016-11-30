1880df635df4098fed8410ba33694d0406cd1202
Aaron Lavers <aaron@IPVentures.com.au>
2016-11-30 13:48:35 +0800

    update main body script

--- js/application.js
+++ js/application.js
@@ -1,49 +1,49 @@
 $(document).ready(function () {
 	
 	// pageload request data
-  	$.ajax({
-  		method: 'POST',
-	    url: 'http://192.168.1.12:8080/4DAction',
-	    headers: {
-	        'User-Name':'Master',
-	        'User-Password':'WDT',
-	        'eConnect-Version':'2.0',
-	        'TypeOfRequest':'Schema'
-	    },
-	    dataType: 'html',
-	    crossDomain:true, 
-	    timeout: 5000,
-	    // Succeffully connected
-	    success: function(data){
-	    	// return success
-	    	console.log('succes: loaded data');
-	      	// console.log('succes: '+data);
-	      	// select menu data and append to side bar
-	      	var menu = $('<div />').append(data).find('#accordion1').html();
-	      	$('#accordion1').html(menu);
-	    },
-	    // unsuccefful connection
-	    error: function(x, t, m) {
-		    if(t==="timeout") {
-		        swal({
-				  title: "Error!",
-				  text: "The API timed out",
-				  type: "error",
-				  confirmButtonText: "Close"
+	$.ajax({
+		method: 'POST',
+		url: 'http://192.168.1.12:8080/4DAction',
+		headers: {
+			'User-Name':'Master',
+			'User-Password':'WDT',
+			'eConnect-Version':'2.0',
+			'TypeOfRequest':'Schema'
+		},
+		dataType: 'html',
+		crossDomain:true, 
+		timeout: 5000,
+		// Succeffully connected
+		success: function(data){
+			// return success
+			console.log('succes: loaded data');
+			// console.log('succes: '+data);
+			// select menu data and append to side bar
+			var menu = $('<div />').append(data).find('#accordion1').html();
+			$('#accordion1').html(menu);
+		},
+		// unsuccefful connection
+		error: function(x, t, m) {
+			if(t==="timeout") {
+				swal({
+					title: "Error!",
+					text: "The API timed out",
+					type: "error",
+					confirmButtonText: "Close"
 				});
-		    } else {
-		        swal({
-				  title: "Error!",
-				  text: "The API is broken or some such",
-				  type: "error",
-				  confirmButtonText: "Close"
+			} else {
+				swal({
+					title: "Error!",
+					text: "The API is broken or some such",
+					type: "error",
+					confirmButtonText: "Close"
 				});
-		    }
+			}
 		}
 	  });
 
 
-  	// Search list
+	// Search list
 	$('#accordion1').btsListFilter('#searchinput', {itemChild: 'a'});
 
 	// Start menu click function
@@ -68,65 +68,57 @@ function requestPage(){
 
 	$(document).on('click','#accordion1 li a',function(e){
 		// prevent href from triggering
-	    e.preventDefault();
-	    // set variables
-	    var link = $(this);
-	    var linkHref = link.attr('href').replace('TableName-','');
-	    var Headers = {
-		    'User-Name':'Master',
-		    'User-Password':'WDT',
-		    'eConnect-Version':'2.0',
-		    'TypeOfRequest':'Schema',
-		    'TableName':linkHref
+		e.preventDefault();
+		// set variables
+		var link = $(this);
+		var linkHref = link.attr('href').replace('TableName-','');
+		var Headers = {
+			'User-Name':'Master',
+			'User-Password':'WDT',
+			'eConnect-Version':'2.0',
+			'TypeOfRequest':'Schema',
+			'TableName':linkHref
 		}
 		// check header data
 		console.log(Headers);
 		// perform rest POST request
-	    $.ajax({
+		$.ajax({
 
-	        method: 'POST',
-		    url: 'http://192.168.1.12:8080/4DAction',
-		    headers: Headers,
-		    dataType: 'html',
-		    crossDomain:true, 
-		    // wait five seconds before timeout
-		    timeout: 5000,
+			method: 'POST',
+			url: 'http://192.168.1.12:8080/4DAction',
+			headers: Headers,
+			dataType: 'html',
+			crossDomain:true, 
+			// wait five seconds before timeout
+			timeout: 5000,
 
-	        success : function(data){
-	            if(data==1){
-	                alert('click is saved OK' + linkHref);
-	                console.log(data);
+			success : function(data){
+				console.log(data);
+				// select main_col data and append
+				var main_col = $('<div />').append(data).find('.main').html();
+				$('.main').html(main_col);
+				
+			},
 
-	            } else if(data==0){
-	                alert("click can't be saved." + linkHref);
-	                console.log(data);
-	                
-	            } else{
-	                alert('error with your code ' + linkHref);
-	                console.log(data);
-
-	            }
-	        },
-
-	        error: function(x, t, m) {
-		        if(t==="timeout") {
-		            swal({
-					  title: "Error!",
-					  text: "The request for " + linkHref + " timed out",
-					  type: "error",
-					  confirmButtonText: "Close"
+			error: function(x, t, m) {
+				if(t==="timeout") {
+					swal({
+						title: "Error!",
+						text: "The request for " + linkHref + " timed out",
+						type: "error",
+						confirmButtonText: "Close"
 					});
-		        } else {
-		            alert(t);
-		            swal({
-					  title: "Error!",
-					  text: "Something went wrong",
-					  type: "error",
-					  confirmButtonText: "Close"
+				} else {
+					alert(t);
+					swal({
+						title: "Error!",
+						text: "Something went wrong",
+						type: "error",
+						confirmButtonText: "Close"
 					});
-		        }
-		    }
-	    });
+				}
+			}
+		});
 
 	});
 
