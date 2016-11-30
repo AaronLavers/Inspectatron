b590df24491465464a59d304aa5aeffc1007bdb3
Aaron Lavers <aaron@IPVentures.com.au>
2016-11-30 12:28:08 +0800

    Update menu click function

--- js/application.js
+++ js/application.js
@@ -23,57 +23,59 @@ $(document).ready(function () {
 	  });
 
 
-
-  	
-
   	// Search list
 	$('#accordion1').btsListFilter('#searchinput', {itemChild: 'a'});
 
-});
+	// Start menu click function
+	requestPage()
 
-$(document).on('click','#accordion1 li a',function(e){
-    e.preventDefault();
-	
-    var link = $(this);
-    var linkHref = link.attr('href').replace('TableName-','');
-    var Headers = {
-	    'User-Name':'Master',
-	    'User-Password':'WDT',
-	    'eConnect-Version':'2.0',
-	    'TypeOfRequest':'Schema',
-	    'TableName':linkHref
-	}
-
-	// console.log(headers);
-
-    $.ajax({
-        method: 'POST',
-	    url: 'http://192.168.1.12:8080/4DAction',
-	    headers: Headers,
-	    dataType: 'html',
-	    crossDomain:true, 
-	    // wait five seconds before timeout
-	    timeout: 5000,
-
-        success : function(data){
-            if(data==1){
-                alert('click is saved OK' + linkHref);
-                console.log(data);
-
-            } else if(data==0){
-                alert("click can't be saved." + linkHref);
-                console.log(data);
-                
-            } else{
-                alert('error with your code ' + linkHref);
-                console.log(data);
-            }
-        }
-    });
 });
 
 
+
+// menu click function
 function requestPage(){
 
+	$(document).on('click','#accordion1 li a',function(e){
+		// prevent href from triggering
+	    e.preventDefault();
+	    // set variables
+	    var link = $(this);
+	    var linkHref = link.attr('href').replace('TableName-','');
+	    var Headers = {
+		    'User-Name':'Master',
+		    'User-Password':'WDT',
+		    'eConnect-Version':'2.0',
+		    'TypeOfRequest':'Schema',
+		    'TableName':linkHref
+		}
+		// check header data
+		console.log(Headers);
+		// perform rest POST request
+	    $.ajax({
+	        method: 'POST',
+		    url: 'http://192.168.1.12:8080/4DAction',
+		    headers: Headers,
+		    dataType: 'html',
+		    crossDomain:true, 
+		    // wait five seconds before timeout
+		    timeout: 5000,
+	        success : function(data){
+	            if(data==1){
+	                alert('click is saved OK' + linkHref);
+	                console.log(data);
+
+	            } else if(data==0){
+	                alert("click can't be saved." + linkHref);
+	                console.log(data);
+	                
+	            } else{
+	                alert('error with your code ' + linkHref);
+	                console.log(data);
+	            }
+	        }
+	    });
+
+	});
 
 }
